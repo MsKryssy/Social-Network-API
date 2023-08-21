2d796d5d7e9edc1b808b5665f212193e5c852f96
Krystal Richardson <krystal.williams2019@gmail.com>
2023-08-21 18:45:31 -0400

    gathering route information

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    removeThought,
+    createThought,
+    updateThought,
+    createReaction,
+    removeReaction,
+    removeReaction,
+} = require('../../controllers/thoughtController');
+router.route('/').get(getThoughts).post(createThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(removeThought);
+router.route('/:thoughtId/reactions/').post(createReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
