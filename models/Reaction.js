520ac7c0df90a9750f7aa938d7b72abd977fa06f
Krystal Richardson <krystal.williams2019@gmail.com>
2023-08-24 12:19:17 -0400

    added accidently deleted file

---
+++ models/Reaction.js
@@ -0,0 +1,30 @@
+const { Schema } = require('mongoose');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => {
+            return new Schema.Types.ObjectId
+        },
+    },
+    reactionBody: {
+        type: String, 
+        required: true,
+        minLength: 1,
+        maxLength: 280,
+    },
+    username: {
+        type: Date,
+        default: Date.now,
+        get: (date) => {
+            return date.toLocaleDateString();
+        }
+    },
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+});    
+ module.exports = reactionsSchema;
