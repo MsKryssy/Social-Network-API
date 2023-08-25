b72c70cde2ec5929c4f59b4d879c108c9dd3447c
Krystal Richardson <krystal.williams2019@gmail.com>
2023-08-25 13:42:44 -0400

    final changes

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-const connectionString =
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialsDB';
+mongoose.set('strictQuery', true);
 
-connect(connectionString);
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialsDB'
+);
 
-module.exports = connection;
+module.exports = mongoose.connection;
